be7bc14e04e221bf10e67b52c019235ba56535b3
Goryoo7 <kiegechev09@gmail.com>
2025-09-10 21:33:12 +0700

    Второй коммит

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from lab1!\n";
+#ifndef PROJECT_VERSION
+#define PROJECT_VERSION "1.0.0"
+#endif
+
+int main() {
+    std::cout << "Hello, World! Version " << PROJECT_VERSION << std::endl;
+    std::cout << "Poputka 2" ;
+    return 0;
 }

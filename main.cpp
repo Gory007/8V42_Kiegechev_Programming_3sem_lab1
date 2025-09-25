50ab2cc45e34c95c3209255a1500f1cde751f64d
Goryoo7 <kiegechev09@gmail.com>
2025-09-25 18:38:14 +0700

    5 commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
-#ifndef VERSION_PROJECT
-#define VERSION_PROJECT "0"
-#endif
-
 int main(int, char**){
-    int a;
     std::cout << "Hello, World! Version " << VERSION_PROJECT;
+    return 0;
 }

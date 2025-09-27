2508d88e7c5268983955550d0553bd0337727136
Goryoo7 <kiegechev09@gmail.com>
2025-09-27 14:56:46 +0700

    19 commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 
 int main(int, char**){
     std::cout << "Hello World! Version 0.1." << VERSION_PROJECT+1 << std::endl;
-    std::cout << "Проверка3";
+    std::cout << "Проверка4";
     return 0;
 }
